c12592ba6d1466cc94f2ca9330aa42957ff1fcf0
hoovler <architect@databuildr.com>
2018-08-21 15:51:44 -0400

    changed the default footer for the theme to remove reference to WordPress..

--- editor-wpcom/footer.php
+++ editor-wpcom/footer.php
@@ -12,7 +12,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a class="powered-by" href="<?php echo esc_url( __( 'http://wordpress.org/', 'editor' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'editor' ), 'WordPress' ); ?></a>
+			<a class="powered-by" href="<?php echo esc_url( __( 'https://github.com/hoovler/databuildr-site', 'dbrSource' ) ); ?>"><?php printf( __( 'Copyright 2018 by %s', 'dbrSource' ), 'Data BuildR, LLC' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %1$s by %2$s', 'editor' ), 'editor', '<a href="https://array.is/">Array</a>' ); ?>
 		</div><!-- .site-info -->
